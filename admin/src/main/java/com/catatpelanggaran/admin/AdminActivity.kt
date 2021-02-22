e343818dad1ef04ea463f0d92820f445588266bf
adriandk <adriandaniel1803@gmail.com>
2021-02-22 16:34:54 +0700

    Bottom  Navigation

--- admin/src/main/java/com/catatpelanggaran/admin/AdminActivity.kt
+++ admin/src/main/java/com/catatpelanggaran/admin/AdminActivity.kt
@@ -2,17 +2,14 @@ package com.catatpelanggaran.admin
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import com.adriandery.catatpelanggaran.LoginActivity
 import com.adriandery.catatpelanggaran.SharedPreferences
 import com.catatpelanggaran.admin.dashboard.DashboardFragment
 import com.catatpelanggaran.admin.profile.EditFragment
-import com.google.android.material.navigation.NavigationView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -21,9 +18,14 @@ import kotlinx.android.synthetic.main.activity_admin.*
 import kotlinx.android.synthetic.main.app_bar_admin.*
 import kotlinx.android.synthetic.main.nav_header_admin.*
 
-class AdminActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class AdminActivity : AppCompatActivity() {
 
     lateinit var nip: String
+    private val homeFrag = DashboardFragment()
+    private val profileFrag = EditFragment()
+    private val fragmentManager = supportFragmentManager
+    var fragment: Fragment = homeFrag
+    var fragmentName = "home"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,22 +35,26 @@ class AdminActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
         nip = intent.getStringExtra("NIP").toString()
         getData(nip)
 
-        val toggle = ActionBarDrawerToggle(
-            this,
-            drawer_layout,
-            toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        )
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
+        fragmentManager.beginTransaction().add(R.id.nav_host_fragment, profileFrag, "2")
+            .hide(profileFrag).commit()
+        fragmentManager.beginTransaction().add(R.id.nav_host_fragment, homeFrag, "1").commit()
 
-        nav_view.setNavigationItemSelectedListener(this)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.nav_host_fragment, DashboardFragment())
-                .commit()
-            supportActionBar?.title = "Dashboard"
+        bottom_nav_admin.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    fragmentName = "home"
+                    fragmentManager.beginTransaction().hide(fragment).show(homeFrag).commit()
+                    fragment = homeFrag
+                    setting_admin.visibility = View.GONE
+                }
+                R.id.profile -> {
+                    fragmentName = "profile"
+                    fragmentManager.beginTransaction().hide(fragment).show(profileFrag).commit()
+                    fragment = profileFrag
+                    setting_admin.visibility = View.VISIBLE
+                }
+            }
+            true
         }
     }
 
@@ -60,8 +66,6 @@ class AdminActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
                 if (snapshot.child(nip).exists()) {
                     val nama = snapshot.child(nip).child("nama").value.toString()
                     admin_name.text = nama
-                    admin_name_nav.text = nama
-                    admin_nip.text = nip
                 } else {
                     val intent = Intent(this@AdminActivity, LoginActivity::class.java)
                     startActivity(intent)
@@ -78,35 +82,35 @@ class AdminActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
 
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        var fragment: Fragment? = null
-        var title = "Dashboard"
-        when (item.itemId) {
-            R.id.nav_dashboard -> {
-                fragment = DashboardFragment()
-                title = "Dashboard"
-            }
-            R.id.nav_edit -> {
-                fragment = EditFragment()
-                title = "Edit"
-            }
-            R.id.nav_logout -> {
-                val intent = Intent(this, LoginActivity::class.java)
-                startActivity(intent)
-                SharedPreferences.clearData(this)
-                finish()
-            }
-        }
-        if (fragment != null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.nav_host_fragment, fragment)
-                .commit()
-        }
-        supportActionBar?.title = title
-
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
-    }
+//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+//        var fragment: Fragment? = null
+//        var title = "Dashboard"
+//        when (item.itemId) {
+//            R.id.nav_dashboard -> {
+//                fragment = DashboardFragment()
+//                title = "Dashboard"
+//            }
+//            R.id.nav_edit -> {
+//                fragment = EditFragment()
+//                title = "Edit"
+//            }
+//            R.id.nav_logout -> {
+//                val intent = Intent(this, LoginActivity::class.java)
+//                startActivity(intent)
+//                SharedPreferences.clearData(this)
+//                finish()
+//            }
+//        }
+//        if (fragment != null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.nav_host_fragment, fragment)
+//                .commit()
+//        }
+//        supportActionBar?.title = title
+//
+//        drawer_layout.closeDrawer(GravityCompat.START)
+//        return true
+//    }
 
     override fun onBackPressed() {
         super.onBackPressed()
